b762b0dd6bd78aee37db2ffbe391dbf781d6c85d
炊逸飞 <YIFEI.CUAN@hand-china.com>
2021-03-15 16:38:06 +0800

    security

---
+++ src/main/java/com/cyf/security/app/service/HelloService.java
@@ -0,0 +1,16 @@
+package com.cyf.security.app.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * description
+ *
+ * @author yifei.cuan@hand-china.com 2021/03/08 17:15
+ */
+@Service
+public class HelloService {
+
+//	@Autowired
+//	PasswordEncoder passwordEncoder;
+
+}
